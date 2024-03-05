3d72e73818b615baad6cd8e079ed492788dfec80
ZoroOneP <122513927+ZoroOneP@users.noreply.github.com>
2024-03-05 16:47:09 +0530

    components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Select } from "./components";
+
+function App() {
+  return (
+    <div className="p-4">
+      <Select />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Multi } from "./Multi";
+
+const Select = () => {
+  return <div>Select</div>;
+};
+
+export default Select;
